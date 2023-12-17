5061bebe253ab396260d27be3e734ff3f33de822
wwc <onechoo1106@gmail.com>
2023-12-17 21:12:15 +1000

    finish session 9 set entity

---
+++ Entities/Product.cs
@@ -0,0 +1,14 @@
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; } // public -> accessor , int -> type , MyProperty -> name
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Price {get; set;} //set long for payment system for later
+        public string PictureUrl { get; set; }
+        public string type { get; set; }
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
